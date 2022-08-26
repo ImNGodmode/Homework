c2d21340edf63d44d73d6f6c537e0fc56fe9bb1f
ImNGodmode <bjames.56@icloud.com>
2022-08-26 15:06:53 -0400

    created functions to build and shuffle deck

--- blackJack/app.js
+++ blackJack/app.js
@@ -6,3 +6,51 @@
 // Each Game MustUse at least 1 element learned in class. IE: Classes, Objects, etc.
 // If 2 player game…P1 and P2 must be able to be controlled by user..No AIs of default computer moves--unless necessary {ie:BlackJack}
 // :rotating_light:WIREFRAME until lunch then execute code afterwards…A game is only as good as its planning.
+
+let dealerSum = 0;
+let yourSum= 0;
+
+let dealerAceCount = 0;
+let yourAceCount = 0;
+
+let hidden;
+let deck;
+let canHit = true;
+
+
+
+const buildDeck = () =>{
+    let values = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k']
+    let types = ['h', 'c', 'd', 's']
+    deck = []
+    for (let i = 0; i < types.length; i++)
+        for (let g = 0; g< values.length; g++)
+        deck.push(values[g] + types[i])
+}
+buildDeck()
+// builds a deck of cards
+const shuffle = () => {
+    for (let i = 0; i < deck.length; i++){
+        let j = Math.floor(Math.random() *deck.length)
+        let temp = deck[i]
+        deck[i] = deck[j]
+        deck[j] = temp
+    }
+}
+ shuffle()
+//  shuffles the cards so they are not in order
+const getValue = () => {
+    let data = car.split('')
+    let value = data[0]
+
+    if (isNaN(value))
+        if (value === 'a'){
+            return 11
+        }else return 10
+}
+const newGame = () => {
+    hidden = deck.pop()
+    dealerSum += getValue(hidden)
+}
+
+console.log(deck)
