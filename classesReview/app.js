9ae93845923949710c653786ba26e8c92f18dab2
ImNGodmode <bjames.56@icloud.com>
2022-08-26 09:12:35 -0400

    created hamster class

---
+++ classesReview/app.js
@@ -0,0 +1,17 @@
+
+class hamster {
+    constructor (name){
+    this.owner = ''
+    this.name ='name'
+    this.price = 15
+}
+wheelrun(){
+    console.log('squeak squeak')
+}
+eatFood(){
+    console.log('nibble nibble')
+}
+getPrice(){
+    return this.price
+}
+}
