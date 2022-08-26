9f26ad9fab43f7cec4aa33e305e944c07c34ae04
ImNGodmode <bjames.56@icloud.com>
2022-08-26 09:28:02 -0400

    created person class

--- classesReview/app.js
+++ classesReview/app.js
@@ -15,3 +15,46 @@ getPrice(){
     return this.price
 }
 }
+class person{
+    constructor(name){
+        this.name = 'name'
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
+    }
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.age
+    }
+    getWeight(){
+        return this.weight
+    }
+    great(){
+        console.log(this.name)
+    }
+    eat(){
+        this.weight++
+        this.mood++
+    }
+    exercise(){
+        this.weight--
+    }
+    ageUp(){
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount +=10
+    }
+    buyHamster(hamster){
+        this.hamsters.push(hamster)
+        this.mood +=10
+        this.bankAccount -= hamster.getprice()
+    }
+}
+
