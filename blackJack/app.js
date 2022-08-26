802067274c880f178495b3ffdd0ed7a17de4fd23
ImNGodmode <bjames.56@icloud.com>
2022-08-26 13:17:57 -0400

    created html and css for game table

---
+++ blackJack/app.js
@@ -0,0 +1,8 @@
+// Use the concepts we’ve learned in class to develop a game of your choice. Take the time in the breakout rooms until 11:30 (when we break for lunch) to find and identify the gaming concepts. There will be a demonstration from scratch of how we can utilize these tools to build the game.
+// GAME Requirements:
+// -Each game must have an Orginal Commit History (aka No Recycled Code from older projects :nerd_face:)
+// -Each game must have a WinState/Lose State
+// Each Game must keep User’s Score
+// Each Game MustUse at least 1 element learned in class. IE: Classes, Objects, etc.
+// If 2 player game…P1 and P2 must be able to be controlled by user..No AIs of default computer moves--unless necessary {ie:BlackJack}
+// :rotating_light:WIREFRAME until lunch then execute code afterwards…A game is only as good as its planning.
