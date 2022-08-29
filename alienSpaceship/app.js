fc1f9eb05ca7c38df0b556f8c1acf0b4c6bd3bd4
ImNGodmode <bjames.56@icloud.com>
2022-08-29 14:14:04 -0400

    added the play function that generates enemys based on lvl

--- alienSpaceship/app.js
+++ alienSpaceship/app.js
@@ -1,11 +1,14 @@
 let lvl = 1
+let enemys = []
 
+
+// create the player
 const player = {
     hull: 20,
     firePower: 5,
-    accuracy: .7,
+    accuracy: 70,
 }
-
+// create a constructor for enemys
 class alien {
     constructor() {
        this.hull= Math.floor(Math.random()*6)
@@ -15,6 +18,17 @@ class alien {
 
 }
 
-const enemy = new alien
-console.log(enemy)
-console.log(player)
+const play = () =>{
+    // create a loop that creats a number of enemys depending on the level
+    for (i = 1; i <= lvl; i++){
+        const enemy = new alien
+        enemys.push(enemy)
+        let enemyship = document.createElement("img")
+        enemyship.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTSzgGJlXRv6NZACaiVu5TnPCTHZ13s203_w&usqp=CAU"
+        document.getElementById("aliens").append(enemyship)
+    } 
+}
+//play()
+// const enemy = new alien
+// console.log(enemys)
+// console.log(player)
