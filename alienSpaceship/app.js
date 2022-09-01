c9e9f2d1d55f11ee6db40e147cecbc33b68d24a3
ImNGodmode <bjames.56@icloud.com>
2022-08-31 20:52:50 -0400

    added a can fight var to turn off buttons if player dies or retreats

--- alienSpaceship/app.js
+++ alienSpaceship/app.js
@@ -43,12 +43,14 @@ const play = () =>{
         enemyship.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTSzgGJlXRv6NZACaiVu5TnPCTHZ13s203_w&usqp=CAU"
         document.getElementById("aliens").append(enemyship)
         player.hull = 20
+        gold = 0
+        canFight = true
     } 
 }
 // create a function that does all of the actions of the fight
 const fight = () => {
     
-    
+    if (canFight === true) {
         if (Math.random()*100 <= player.accuracy){
             enemys[0].hull -= player.firePower
             info.innerText = `You hit an aliean for ${player.firePower}`
@@ -62,6 +64,7 @@ const fight = () => {
                 info.innerText += `you were hit for ${enemys[0].firePower} damage`
                 if (player.hull <= 0){
                     info.innerText += `you lost at level ${lvl}`
+                    canFight = false
                 }
             }  
         }else if (enemys[0].hull <=0){ 
@@ -74,54 +77,65 @@ const fight = () => {
                 }
         }
             
+    }
         // console.log(enemys)
     // console.log(player)
     // console.log("i am level " + lvl)
 }
 
 const keepFighting = () => {
-    lvl = lvl + 1
-    level.innerHTML = `level ${lvl}`
-    for (i = 1; i <= lvl; i++){
-        const enemy = new alien
-        enemys.push(enemy)
-        const enemyship = document.createElement("img")
-        enemyship.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTSzgGJlXRv6NZACaiVu5TnPCTHZ13s203_w&usqp=CAU"
-        document.getElementById("aliens").append(enemyship)
-        info.innerText = `There are ${lvl} more aliens!`
-    } 
+    if (canFight === true){
+        lvl = lvl + 1
+        level.innerHTML = `level ${lvl}`
+        for (i = 1; i <= lvl; i++){
+            const enemy = new alien
+            enemys.push(enemy)
+            const enemyship = document.createElement("img")
+            enemyship.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTSzgGJlXRv6NZACaiVu5TnPCTHZ13s203_w&usqp=CAU"
+            document.getElementById("aliens").append(enemyship)
+            info.innerText = `There are ${lvl} more aliens!`
+        } 
+    }
 }
 const retreat = () => {
+    if (canFight === true){
     info.innerText = `You escaped at level ${lvl}`
+    canFight = false
+    }
 }
 const rechargeShields = () => {
-   if (gold >= (100 + (100 * rechargeNum))){
-    player.hull = player.maxHull
-    gold -= (100 + (100 * rechargeNum))
-    rechargeNum++
-    health.innerHTML = `health ${player.hull}`
-    coins.innerHTML = `Gold ${gold}`
-   }
+    if (canFight === true) {
+        if (gold >= (100 + (100 * rechargeNum))){
+            player.hull = player.maxHull
+            gold -= (100 + (100 * rechargeNum))
+            rechargeNum++
+            health.innerHTML = `health ${player.hull}`
+            coins.innerHTML = `Gold ${gold}`
+        }
+    }
 }
 const upgradeHull = () => {
-    if (gold >= (250 + (250 * hullUpgrades))){
-    player.maxHull += 5
-    player.hull += 5
-    gold -= (250 + (250 * hullUpgrades)) 
-    hullUpgrades++
-    health.innerHTML = `health ${player.hull}`
-    coins.innerHTML = `Gold ${gold}`
+    if (canFight === true) {
+        if (gold >= (250 + (250 * hullUpgrades))){
+        player.maxHull += 5
+        player.hull += 5
+        gold -= (250 + (250 * hullUpgrades)) 
+        hullUpgrades++
+        health.innerHTML = `health ${player.hull}`
+        coins.innerHTML = `Gold ${gold}`
+        }
     }
 }
 const upgradeCannons = () => {
-    if (gold >= (250 + (250 * gunUpgrades))){
-        player.firePower += 1
-        gold -= (250 + (250 * gunUpgrades)) 
-        gunUpgrades++
-        cannons.innerHTML = `cannons ${player.firePower}`
-        coins.innerHTML = `Gold ${gold}`
+    if (canFight === true) {
+        if (gold >= (250 + (250 * gunUpgrades))){
+            player.firePower += 1
+            gold -= (250 + (250 * gunUpgrades)) 
+            gunUpgrades++
+            cannons.innerHTML = `cannons ${player.firePower}`
+            coins.innerHTML = `Gold ${gold}`
         }
-
+    }
 }
 // console.log(enemys[0].hull)
 //console.log(player)
