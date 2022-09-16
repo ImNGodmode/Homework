857b308bcc1eca59c603bfb14953cb6739e53213
ImNGodmode <bjames.56@icloud.com>
2022-09-16 19:07:45 -0400

    my server is set up and running

---
+++ Pokemon_app/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to the Pokemon App!</h1>')
+})
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("listning on port 3000")
+})
