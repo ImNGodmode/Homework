c7aca3e56a32a6679ee84ff90e20d538981f4845
ImNGodmode <bjames.56@icloud.com>
2022-09-16 19:15:51 -0400

    connected my database, can see json in the browser

--- Pokemon_app/server.js
+++ Pokemon_app/server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const app = express()
-
+const pokemon = require("./models/pokemon.js")
 
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to the Pokemon App!</h1>')
+})
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
 })
 
 
