05a494120246c9e4548373f78f4b9f01ec08bfe0
ImNGodmode <bjames.56@icloud.com>
2022-09-22 00:04:07 -0400

    Created show views of each pokemon

--- Pokemon_app/views/Show.jsx
+++ Pokemon_app/views/Show.jsx
@@ -2,10 +2,15 @@ const React = require('react')
 class Show extends React.Component {
    render () {
     const poke = this.props.poke
+ 
     return (
         <div>
-        <h1> Show Page </h1>
-         {poke.name}
+            <h1> Gotta Catch 'Em all </h1>
+            {/* <h2>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</h2> */}
+            <img src={poke.img + '.jpg'} alt="" width={300} height={300}/>
+            
+
+            <a href='http://localhost:3000/pokemon'>Back</a>
         </div>
      );
     }
